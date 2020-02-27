b075afcec060407f397583c735311ba767a4329a
freedmand <freedmand@gmail.com>
2020-02-27 12:13:34 -0500

    v1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="listcrunch",
-    version="0.2.0",
+    version="1.0.0",
     author="Dylan Freedman",
     author_email="freedmand@gmail.com",
     description="A simple human-readable way to compress redundant sequential data",
